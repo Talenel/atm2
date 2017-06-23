4d12240a287f3704dbbaca097b084625e3a55b42
Justin Gross <jmbg144@gmail.com>
2017-06-23 16:53:34 -0400

    Added minor CSS changes, logout button, updated sql file

--- SQL Files/accountstuff.sql
+++ SQL Files/accountstuff.sql
@@ -1,6 +1,9 @@
 Insert into account(acct_num, balance, user_name) values ('101', '20000.00','user1');
 Insert into account(acct_num, balance, user_name) values ('100', '15000.00','user2');
 
-desc account;
+Insert into transaction(acct_num,action,amt,reason) values ('101','Deposit','20000.00', 'Initial Deposit');
+Insert into transaction(acct_num,action,amt,reason) values ('100','Deposit','15000.00', 'Initial Deposit');
+
+desc transaction;
 
 select * from account;
